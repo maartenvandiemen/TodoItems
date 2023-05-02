09a1965bb8ee8eb259091700e7b63d3bab4d9d75
Maarten van Diemen <maartenvandiemen@msn.com>
2023-05-02 20:08:18 +0200

    Renamed tests to integration tests Implemented testcontainers

--- TodoItems.Api.UnitTests/TodoItemsApiApplication.cs
+++ TodoItems.Api.UnitTests/TodoItemsApiApplication.cs
@@ -5,19 +5,31 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
 using System.Linq;
+using Testcontainers.MsSql;
 
-namespace TodoItems.Api.UnitTests
+namespace TodoItems.Api.IntegrationTests
 {
     internal class TodoItemsApiApplication : WebApplicationFactory<Program>
     {
+        private readonly MsSqlContainer? _container;
+
         private readonly Environment _environment;
 
         public IConfiguration Configuration { get; private set; } = default!;
 
-        public TodoItemsApiApplication(Environment environment) : base() => _environment = environment;
+        public TodoItemsApiApplication(Environment environment, MsSqlContainer? container) : base() 
+        { 
+            _environment = environment;
+
+            if (_environment == Environment.TestContainers)
+            {
+                _container = container ?? throw new ArgumentNullException(nameof(environment));
+            }
+        }
 
         //Use CreateHost for overriding AppSettings: https://github.com/dotnet/AspNetCore.Docs/issues/25364
         protected override IHost CreateHost(IHostBuilder builder)
@@ -34,10 +46,12 @@ namespace TodoItems.Api.UnitTests
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            if (_environment == Environment.Development)
+            if (_environment == Environment.InMemory || _environment == Environment.TestContainers)
             {
                 builder.ConfigureTestServices(services =>
                 {
+                    services.AddLogging(c => c.AddConsole());
+
                     //First remove the serviceDescriptor added in the Program.cs if available
                     var descriptor = services.SingleOrDefault(
                         d => d.ServiceType ==
@@ -48,10 +62,17 @@ namespace TodoItems.Api.UnitTests
                         services.Remove(descriptor);
                     }
 
-                    //Add a new InMemoryDatabase each time this class is initialized.
-                    //We give the database each time a new name in order to ensure that tests can be executed in parallel.
-                    //Lifetime is singleton within this class. Since some tests might create multiple Scopes the Singleton is the safest.
-                    services.AddDbContext<TodoDb>(options => options.UseInMemoryDatabase(Guid.NewGuid().ToString()), ServiceLifetime.Singleton);
+                    if (_environment == Environment.InMemory)
+                    {
+                        //Add a new InMemoryDatabase each time this class is initialized.
+                        //We give the database each time a new name in order to ensure that tests can be executed in parallel.
+                        //Lifetime is singleton within this class. Since some tests might create multiple Scopes the Singleton is the safest.
+                        services.AddDbContext<TodoDb>(options => options.UseInMemoryDatabase(Guid.NewGuid().ToString()), ServiceLifetime.Singleton);
+                    }
+                    if(_environment == Environment.TestContainers)
+                    {
+                        services.AddDbContext<TodoDb>(options => options.UseSqlServer(_container!.GetConnectionString()), ServiceLifetime.Singleton);
+                    }
                 });
             }
 
